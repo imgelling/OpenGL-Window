38aa564e61ed7711664dd412f64d03c0482d423f
ImGelling <47453119+imgelling@users.noreply.github.com>
2022-08-15 17:31:07 -0400

    making it nice
    * Started making it a game:: class
    + added GameErrors

--- OpenGL-Window/Main.cpp
+++ OpenGL-Window/Main.cpp
@@ -1,5 +1,8 @@
 #include <windows.h>
 #include <gl/GL.h>
+#include <iostream>
+// below needs shortened
+#include "../../../Programming/GameLib/GameErrors.h"
 
 #if defined(_WIN32) && !defined(__MINGW32__)
 #pragma comment(lib, "user32.lib")
@@ -7,73 +10,149 @@
 #pragma comment(lib, "opengl32.lib")
 #endif
 
-#include <iostream>
 bool close = false;
-static LRESULT CALLBACK olc_WindowEvent(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+
+namespace game
 {
-	switch (uMsg)
+	class GameWindow
 	{
-		//case WM_MOUSEMOVE:
-		//{
-		//	// Thanks @ForAbby (Discord)
-		//	uint16_t x = lParam & 0xFFFF; uint16_t y = (lParam >> 16) & 0xFFFF;
-		//	int16_t ix = *(int16_t*)&x;   int16_t iy = *(int16_t*)&y;
-		//	ptrPGE->olc_UpdateMouse(ix, iy);
-		//	return 0;
-		//}
-		//case WM_SIZE:       ptrPGE->olc_UpdateWindowSize(lParam & 0xFFFF, (lParam >> 16) & 0xFFFF);	return 0;
-		//case WM_MOUSEWHEEL:	ptrPGE->olc_UpdateMouseWheel(GET_WHEEL_DELTA_WPARAM(wParam));           return 0;
-		//case WM_MOUSELEAVE: ptrPGE->olc_UpdateMouseFocus(false);                                    return 0;
-		//case WM_SETFOCUS:	ptrPGE->olc_UpdateKeyFocus(true);                                       return 0;
-		//case WM_KILLFOCUS:	ptrPGE->olc_UpdateKeyFocus(false);                                      return 0;
-		//case WM_KEYDOWN:	ptrPGE->olc_UpdateKeyState(mapKeys[wParam], true);                      return 0;
-		//case WM_KEYUP:		ptrPGE->olc_UpdateKeyState(mapKeys[wParam], false);                     return 0;
-		//case WM_SYSKEYDOWN: ptrPGE->olc_UpdateKeyState(mapKeys[wParam], true);						return 0;
-		//case WM_SYSKEYUP:	ptrPGE->olc_UpdateKeyState(mapKeys[wParam], false);						return 0;
-		//case WM_LBUTTONDOWN:ptrPGE->olc_UpdateMouseState(0, true);                                  return 0;
-		//case WM_LBUTTONUP:	ptrPGE->olc_UpdateMouseState(0, false);                                 return 0;
-		//case WM_RBUTTONDOWN:ptrPGE->olc_UpdateMouseState(1, true);                                  return 0;
-		//case WM_RBUTTONUP:	ptrPGE->olc_UpdateMouseState(1, false);                                 return 0;
-		//case WM_MBUTTONDOWN:ptrPGE->olc_UpdateMouseState(2, true);                                  return 0;
-		//case WM_MBUTTONUP:	ptrPGE->olc_UpdateMouseState(2, false);                                 return 0;
-	case WM_CLOSE:		close = true;// ptrPGE->olc_Terminate();                                                return 0;
-	case WM_DESTROY:	PostQuitMessage(0); DestroyWindow(hWnd);								return 0;
+	public:
+		GameWindow();
+		GameError LastError();
+		bool SetWindowInfo(std::string name, const int width, const int height, const bool fullScreen, const bool borderless);
+		bool CreateTheWindow();
+		void DoMessagePump();
+	private:
+		std::string _windowTitle;
+		uint32_t _windowWidth;
+		uint32_t _windowHeight;
+		bool _isFullScreen;
+		bool _isBorderless;
+		GameError _lastError;
+
+		// Windows only stuff
+		HWND _windowHandle;
+		static LRESULT CALLBACK WindowEventProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	};
+
+	GameWindow::GameWindow()
+	{
+		_windowWidth = 0;
+		_windowHeight = 0;
+		_isFullScreen = false;
+		_isBorderless = false;
+		_windowHandle = NULL;
 	}
-	return DefWindowProc(hWnd, uMsg, wParam, lParam);
-}
 
-int main()
-{
-	WNDCLASS wc{};
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
-	wc.hInstance = GetModuleHandle(nullptr);
-	wc.lpfnWndProc = olc_WindowEvent;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.lpszMenuName = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszClassName = L"Game Engine";
-	RegisterClass(&wc);
-
-	DWORD dwExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;
-	DWORD dwStyle = WS_CAPTION | WS_SYSMENU | WS_VISIBLE | WS_THICKFRAME;
-	HWND win;
-	win = CreateWindowEx(dwExStyle, L"Game Engine", L"", dwStyle,
-		0, 0, 500, 500, NULL, NULL, GetModuleHandle(nullptr), NULL/*needs to be "this" in a class*/);
-	
-	MSG msg;
-	do
+	GameError GameWindow::LastError()
 	{
+		return _lastError;
+	}
+	
+	LRESULT CALLBACK GameWindow::WindowEventProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		switch (uMsg)
+		{
+			//case WM_MOUSEMOVE:
+			//{
+			//	// Thanks @ForAbby (Discord)
+			//	uint16_t x = lParam & 0xFFFF; uint16_t y = (lParam >> 16) & 0xFFFF;
+			//	int16_t ix = *(int16_t*)&x;   int16_t iy = *(int16_t*)&y;
+			//	ptrPGE->olc_UpdateMouse(ix, iy);
+			//	return 0;
+			//}
+			//case WM_SIZE:       ptrPGE->olc_UpdateWindowSize(lParam & 0xFFFF, (lParam >> 16) & 0xFFFF);	return 0;
+			//case WM_MOUSEWHEEL:	ptrPGE->olc_UpdateMouseWheel(GET_WHEEL_DELTA_WPARAM(wParam));           return 0;
+			//case WM_MOUSELEAVE: ptrPGE->olc_UpdateMouseFocus(false);                                    return 0;
+			//case WM_SETFOCUS:	ptrPGE->olc_UpdateKeyFocus(true);                                       return 0;
+			//case WM_KILLFOCUS:	ptrPGE->olc_UpdateKeyFocus(false);                                      return 0;
+			//case WM_KEYDOWN:	ptrPGE->olc_UpdateKeyState(mapKeys[wParam], true);                      return 0;
+			//case WM_KEYUP:		ptrPGE->olc_UpdateKeyState(mapKeys[wParam], false);                     return 0;
+			//case WM_SYSKEYDOWN: ptrPGE->olc_UpdateKeyState(mapKeys[wParam], true);						return 0;
+			//case WM_SYSKEYUP:	ptrPGE->olc_UpdateKeyState(mapKeys[wParam], false);						return 0;
+			//case WM_LBUTTONDOWN:ptrPGE->olc_UpdateMouseState(0, true);                                  return 0;
+			//case WM_LBUTTONUP:	ptrPGE->olc_UpdateMouseState(0, false);                                 return 0;
+			//case WM_RBUTTONDOWN:ptrPGE->olc_UpdateMouseState(1, true);                                  return 0;
+			//case WM_RBUTTONUP:	ptrPGE->olc_UpdateMouseState(1, false);                                 return 0;
+			//case WM_MBUTTONDOWN:ptrPGE->olc_UpdateMouseState(2, true);                                  return 0;
+			//case WM_MBUTTONUP:	ptrPGE->olc_UpdateMouseState(2, false);                                 return 0;
+		case WM_CLOSE:		close = true;// ptrPGE->olc_Terminate();                                                return 0;
+		case WM_DESTROY:	PostQuitMessage(0); DestroyWindow(hWnd);								return 0;
+		}
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
+
+	bool GameWindow::SetWindowInfo(std::string name, const int width, const int height, const bool fullScreen, const bool borderless)
+	{
+		_windowTitle = name;
+		_windowWidth = width;
+		_windowHeight = height;
+		_isFullScreen = fullScreen;
+		_isBorderless = borderless;
+
+		return true;
+	}
+
+	bool GameWindow::CreateTheWindow()
+	{
+		WNDCLASS wc{};
+		wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+		wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+		wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+		wc.hInstance = GetModuleHandle(nullptr);
+		wc.lpfnWndProc = WindowEventProc;
+		wc.cbClsExtra = 0;
+		wc.cbWndExtra = 0;
+		wc.lpszMenuName = nullptr;
+		wc.hbrBackground = nullptr;
+		wc.lpszClassName = (LPWSTR)_windowTitle.c_str();
+		RegisterClass(&wc);
+
+		DWORD dwExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;
+		DWORD dwStyle = WS_CAPTION | WS_SYSMENU | WS_VISIBLE | WS_THICKFRAME;
+
+		_windowHandle = CreateWindowEx(dwExStyle, (LPWSTR)_windowTitle.c_str(), L"", dwStyle,
+			0, 0, _windowWidth, _windowHeight, NULL, NULL, GetModuleHandle(nullptr), this);
+		if (!_windowHandle)
+		{
+			_lastError = { GameErrors::GameWindowsSpecific, "Windows Error Number : "+std::to_string(GetLastError())};
+			return false;
+		}
+
+		return true;
+	}
+
+	void GameWindow::DoMessagePump(void)
+	{
+		MSG msg;
 		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) > 0)
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
-			std::cout << "work?";
-	} while (!close);
+	}
+}
 
+int main()
+{
+	game::GameWindow window;
+	game::GameError error;
+
+	if (!window.SetWindowInfo("Test Name", 1280, 720, false, false))
+	{
+		std::cout << window.LastError();
+	}
+	//error = window.CreateTheWindow();
+	if (!window.CreateTheWindow())
+	{
+		std::cout << window.LastError();
+	}
+
+	// "Game Loop"
+	do
+	{
+		window.DoMessagePump();
+	} while (!close);
 
 	return 0;
 }
