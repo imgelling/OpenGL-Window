e683835b57fd1f727e14b43be01c7e86555f9183
ImGelling <47453119+imgelling@users.noreply.github.com>
2022-08-15 12:08:24 -0400

    Update Main.cpp
    + Creating a simple window

--- OpenGL-Window/Main.cpp
+++ OpenGL-Window/Main.cpp
@@ -1,6 +1,65 @@
-#include <iostream>
+#include <windows.h>
 
-int main(void)
+static LRESULT CALLBACK olc_WindowEvent(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	switch (uMsg)
+	{
+	//case WM_MOUSEMOVE:
+	//{
+	//	// Thanks @ForAbby (Discord)
+	//	uint16_t x = lParam & 0xFFFF; uint16_t y = (lParam >> 16) & 0xFFFF;
+	//	int16_t ix = *(int16_t*)&x;   int16_t iy = *(int16_t*)&y;
+	//	ptrPGE->olc_UpdateMouse(ix, iy);
+	//	return 0;
+	//}
+	//case WM_SIZE:       ptrPGE->olc_UpdateWindowSize(lParam & 0xFFFF, (lParam >> 16) & 0xFFFF);	return 0;
+	//case WM_MOUSEWHEEL:	ptrPGE->olc_UpdateMouseWheel(GET_WHEEL_DELTA_WPARAM(wParam));           return 0;
+	//case WM_MOUSELEAVE: ptrPGE->olc_UpdateMouseFocus(false);                                    return 0;
+	//case WM_SETFOCUS:	ptrPGE->olc_UpdateKeyFocus(true);                                       return 0;
+	//case WM_KILLFOCUS:	ptrPGE->olc_UpdateKeyFocus(false);                                      return 0;
+	//case WM_KEYDOWN:	ptrPGE->olc_UpdateKeyState(mapKeys[wParam], true);                      return 0;
+	//case WM_KEYUP:		ptrPGE->olc_UpdateKeyState(mapKeys[wParam], false);                     return 0;
+	//case WM_SYSKEYDOWN: ptrPGE->olc_UpdateKeyState(mapKeys[wParam], true);						return 0;
+	//case WM_SYSKEYUP:	ptrPGE->olc_UpdateKeyState(mapKeys[wParam], false);						return 0;
+	//case WM_LBUTTONDOWN:ptrPGE->olc_UpdateMouseState(0, true);                                  return 0;
+	//case WM_LBUTTONUP:	ptrPGE->olc_UpdateMouseState(0, false);                                 return 0;
+	//case WM_RBUTTONDOWN:ptrPGE->olc_UpdateMouseState(1, true);                                  return 0;
+	//case WM_RBUTTONUP:	ptrPGE->olc_UpdateMouseState(1, false);                                 return 0;
+	//case WM_MBUTTONDOWN:ptrPGE->olc_UpdateMouseState(2, true);                                  return 0;
+	//case WM_MBUTTONUP:	ptrPGE->olc_UpdateMouseState(2, false);                                 return 0;
+	//case WM_CLOSE:		ptrPGE->olc_Terminate();                                                return 0;
+	case WM_DESTROY:	PostQuitMessage(0); DestroyWindow(hWnd);								return 0;
+	}
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+int main()
+{
+	WNDCLASS wc{};
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wc.hInstance = GetModuleHandle(nullptr);
+	wc.lpfnWndProc = olc_WindowEvent;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.lpszMenuName = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszClassName = L"OLC_PIXEL_GAME_ENGINE";
+	RegisterClass(&wc);
+
+	DWORD dwExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;
+	DWORD dwStyle = WS_CAPTION | WS_SYSMENU | WS_VISIBLE | WS_THICKFRAME;
+	HWND win;
+	win = CreateWindowEx(dwExStyle, L"OLC_PIXEL_GAME_ENGINE", L"", dwStyle,
+		0, 0, 500, 500, NULL, NULL, GetModuleHandle(nullptr), NULL/*needs to be "this" in a class*/);
+	
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
 	return 0;
 }
