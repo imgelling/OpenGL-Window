ebae68b11eebb35a675ac06e9aba2df9b1a019fa
ImGelling <47453119+imgelling@users.noreply.github.com>
2022-08-28 05:30:14 -0400

    Moved methods from PerformanceTimer to cpp file

--- OpenGL-Window/GamePerformanceTimer.h
+++ OpenGL-Window/GamePerformanceTimer.h
@@ -28,105 +28,29 @@ namespace game
 			_timedFuncs.clear();
 		}
 		// Returns number of times the function was timed, -1 if not found
-		uint32_t Ticks(const std::string name)
-		{
-			_mutex.lock();
-			unsigned int ret = 0;
-			try
-			{
-				ret = _timedFuncs.at(name)._ticks;
-			}
-			catch (...)
-			{
-				ret = -1;
-			}
-			_mutex.unlock();
-			return ret;
-		}
+		uint32_t Ticks(const std::string name);
+
 		// Returns the nanoseconds the function took on the last call, -1 if not found
-		double LastRun(const std::string name)
-		{
-			_mutex.lock();
-			double ret = 0.0;
-			try
-			{
-				ret = _timedFuncs.at(name)._time;
-			}
-			catch (...)
-			{
-				ret = -1.0;
-			}
-			_mutex.unlock();
-			return ret;
-		}
+		double LastRun(const std::string name);
+
 		// Returns the total nanoseconds the function ran over all the calls, -1 if not found
-		double Cumulative(const std::string name)
-		{
-			_mutex.lock();
-			double ret = 0.0;
-			try
-			{
-				ret = _timedFuncs.at(name)._cumulative;
-			}
-			catch (...)
-			{
-				ret = -1.0;
-			}
-			_mutex.unlock();
-		}
+		double Cumulative(const std::string name);
+
 		// Returns the average nanoseconds the function ran over all the calls, -1 if not found
-		double Average(const std::string name)
-		{
-			_mutex.lock();
-			double ret = 0.0;
-			try
-			{
-				ret = _timedFuncs.at(name)._cumulative / _timedFuncs.at(name)._ticks;
-			}
-			catch (...)
-			{
-				ret = -1.0;
-			}
-			_mutex.unlock();
-			return ret;
-		}
+		double Average(const std::string name);
+
 		// Returns an unmodifiable container of timed functions
 		const std::unordered_map <std::string, _FunctionToTime>& TimedFunctions()
 		{
 			return _timedFuncs;
 		}
-		// Start timing
-		void Start(const std::string name)
-		{
-			_FunctionToTime t;
-			_mutex.lock();
-			_timedFuncs.try_emplace(name, t);
-			_timedFuncs[name]._name = name;
-			_timedFuncs[name]._start = std::chrono::high_resolution_clock::now();
-			_timedFuncs[name]._ticks++;
-			_mutex.unlock();
-		};
-		// Stop timing
-		void Stop(const std::string name)
-		{
-#define duration(a) std::chrono::duration_cast<std::chrono::nanoseconds>(a).count()
-#define timeNow() std::chrono::high_resolution_clock::now()
-			_mutex.lock();
-			try
-			{
-				_timedFuncs.at(name)._time = (double)(duration(timeNow() - _timedFuncs.at(name)._start));
-				_timedFuncs.at(name)._cumulative += _timedFuncs[name]._time;
-			}
-			catch (...)
-			{
-				// Output to a log or something
-				//std::cout << __func__ << ": \"" << name << "\" is not found." << "\n";
-			}
-			_mutex.unlock();
 
-#undef duration
-#undef timeNow
-		};
+		// Start timing
+		void Start(const std::string name);
+
+		// Stop timing
+		void Stop(const std::string name);
+
 	};
 
 
