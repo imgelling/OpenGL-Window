ed51a689678556c5a7a008f0510fbb7a4ab47462
ImGelling <47453119+imgelling@users.noreply.github.com>
2022-09-09 16:25:50 -0400

    Added more Vector2 operators

--- OpenGL-Window/GameMath.h
+++ OpenGL-Window/GameMath.h
@@ -3,32 +3,56 @@
 namespace game
 {
 
-	// Vector2 
+#pragma region Vector2 
 	template <typename T>
 	class Vector2
 	{
 	public:
 		T x;
 		T y;
-		Vector2() : x((T)0.0), y((T)0.0) {};
+		Vector2()  
+		{
+			x = (T)0.0;
+			y = (T)0.0;
+		};
 		Vector2(const T& x, const T& y)
 		{
 			this->x = x;
 			this->y = y;
 		}
-		Vector2 operator- (const Vector2& rhs)
+		Vector2 operator - (const Vector2& rhs)
 		{
 			Vector2<T> ret;
 			ret.x = x - rhs.x;
 			ret.y = y - rhs.y;
 			return ret;
 		}
+		Vector2& operator -= (const Vector2& rhs)
+		{
+			x = x - rhs.x;
+			y = y - rhs.y;
+			return *this;
+		}
+		Vector2 operator + (const Vector2& rhs)
+		{
+			Vector2<T> ret;
+			ret.x = x + rhs.x;
+			ret.y = y + rhs.y;
+			return ret;
+		}
+		Vector2& operator += (const Vector2& rhs)
+		{
+			x = x + rhs.x;
+			y = y + rhs.y;
+			return *this;
+		}
+
 	private:
 	};
-	typedef Vector2<int> Vector2i;	
-	typedef Vector2<float> Vector2f;
-	typedef Vector2<double> Vector2d;
-	typedef Vector2<int> Pointi;
-	typedef Vector2<float> Pointf;
-	typedef Vector2<double> Pointd;
+	typedef Vector2<int> Vector2i, Pointi;	
+	typedef Vector2<float> Vector2f, Pointf;
+	typedef Vector2<double> Vector2d, Pointd;
+#pragma endregion
+
+
 }
