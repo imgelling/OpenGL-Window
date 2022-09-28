2cbc6939cd735141aeaeb1c7bae5424a93a867a8
ImGelling <47453119+imgelling@users.noreply.github.com>
2022-09-28 12:35:00 -0400

    Pixel Mode done
    may need a tiny bit of code cleanup

--- OpenGL-Window/GamePixelMode.h
+++ OpenGL-Window/GamePixelMode.h
@@ -2,6 +2,8 @@
 #include <gl/GL.h>
 #include "GameMath.h"
 #include "GameErrors.h"
+#include "GameEngine.h"
+#include "GameTexture2D.h"
 
 namespace game
 {
@@ -11,25 +13,37 @@ namespace game
 	//  Needs a color class
 	//  Needs a working copy of frame
 	//
-	extern GameError _lastError;
+	extern GameError lastError;
+	extern Engine* enginePointer;
 	class PixelMode
 	{
 	public:
 		PixelMode();
 		~PixelMode();
 
-		bool Initialize(const Vector2i &sizeOfScreen);
+		bool Initialize(const Vector2i& sizeOfScreen);
+		void Render();
+		void Clear(const Color &color);
+		void Clear();
+		void Pixel(const uint32_t x, const uint32_t y, const game::Color& color);
+
+
 	private:
+		void _UpdateFrameBuffer();
 		Texture2dGL _frameBuffer[2];
+		void _ScaleTextureToWindow();
 		uint32_t _compiledQuad;
 		uint32_t* _video;
 		Vector2i _bufferSize;
+		Vector2i _windowSize;
+		uint32_t _currentBuffer;
 	};
 
 	inline PixelMode::PixelMode()
 	{
 		_compiledQuad = 0;
 		_video = nullptr;
+		_currentBuffer = 0;
 	}
 
 	inline PixelMode::~PixelMode()
@@ -41,16 +55,159 @@ namespace game
 	{
 		_bufferSize = sizeOfScreen;
 
+		// Save window size
+		_windowSize = enginePointer->GetWindowSize();
+
 		// Create video buffer
 		_video = new uint32_t[_bufferSize.width * _bufferSize.height];
 		if (_video == nullptr)
 		{
-			_lastError = { GameErrors::GameRenderer, "Could not allocate RAM for video buffer." };
+			lastError = { GameErrors::GameRenderer, "Could not allocate RAM for video buffer." };
 			return false;
 		}
 
 		// Create frame buffer textures
+		for (uint32_t loop = 0; loop < 2; loop++)
+		{
+			_frameBuffer[loop].width = _bufferSize.width;
+			_frameBuffer[loop].height = _bufferSize.height;
+			_frameBuffer[loop].componentsPerPixel = 4;
+			_frameBuffer[loop].filterType = game::TextureFilterType::Point;
+			if (!enginePointer->CreateTexture(_frameBuffer[loop]))
+			{
+				return false;
+			}
+		}
+
+		// Generate the display list
+		_compiledQuad = glGenLists(1);
+
+		// Scale the texture to window size
+		_ScaleTextureToWindow();
 
 		return true;
+	}
+
+	inline void PixelMode::_UpdateFrameBuffer()
+	{
+		// needs to double buffer
+		glBindTexture(GL_TEXTURE_2D, _frameBuffer[_currentBuffer].bind);
+
+		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, _frameBuffer[_currentBuffer].width, _frameBuffer[_currentBuffer].height, GL_RGBA, game::systemInfo.gpuInfo.internalPixelType, (GLvoid*)_video);  // for intel 
+		glBindTexture(GL_TEXTURE_2D, 0);
+		
+		// Swap texture buffer to draw to
+		_currentBuffer++;
+		if (_currentBuffer > 1) _currentBuffer = 0;
+	}
+
+	inline void PixelMode::_ScaleTextureToWindow()
+	{
+
+		game::Vector2f positionOfScaledTexture;
+		game::Vector2f scale;
+		game::Vector2f sizeOfScaledTexture;
+		float_t tempScale = 0.0f;
+		if (_windowSize.height < _windowSize.width)
+		{
+			scale.y = (float_t)_windowSize.height * _frameBuffer[_currentBuffer].oneOverHeight;
+			tempScale = (float_t)_windowSize.width * _frameBuffer[_currentBuffer].oneOverWidth;
+			if (tempScale > scale.y)
+			{
+				scale.x = scale.y;
+			}
+			else
+			{
+				scale.x = scale.y = tempScale;
+				positionOfScaledTexture.y = ((_windowSize.height >> 1) - ((float_t)_frameBuffer[_currentBuffer].height * scale.y / 2.0f));
+			}
+			positionOfScaledTexture.x = ((_windowSize.width >> 1) - ((float_t)_frameBuffer[_currentBuffer].width * scale.x / 2.0f));
+		}
+		else if (_windowSize.height > _windowSize.width)
+		{
+			scale.x = (float_t)_windowSize.width * _frameBuffer[_currentBuffer].oneOverWidth;
+			scale.y = scale.x;
+			positionOfScaledTexture.y = ((_windowSize.height >> 1) - ((float_t)_frameBuffer[_currentBuffer].height * scale.y / 2.0f));
+		}
+		else
+		{
+			scale = { 1.0f, 1.0f };
+		}
+
+		// Set the size of the scaled texture
+		sizeOfScaledTexture.width = positionOfScaledTexture.x + (_frameBuffer[_currentBuffer].width * scale.x);
+		sizeOfScaledTexture.height = positionOfScaledTexture.y + (_frameBuffer[_currentBuffer].height * scale.y);
+
+		// Homoginize the scaled rect to -1 to 1 range using
+		// position.x = position.x * 2.0 / width - 1.0
+		// position.y = position.y * 2.0 / height - 1.0;
+		positionOfScaledTexture.x = (positionOfScaledTexture.x * 2.0f / (float_t)_windowSize.width) - 1.0f;
+		positionOfScaledTexture.y = (positionOfScaledTexture.y * 2.0f / (float_t)_windowSize.height) - 1.0f;
+		sizeOfScaledTexture.width = ((float_t)sizeOfScaledTexture.width * 2.0f / (float_t)_windowSize.width) - 1.0f;
+		sizeOfScaledTexture.height = ((float_t)sizeOfScaledTexture.height * 2.0f / (float_t)_windowSize.height) - 1.0f;
+		
+		glNewList(_compiledQuad, GL_COMPILE);
+		{
+			glBegin(GL_QUADS);
+			//bl
+			glColor3f(1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0, 0);
+			glVertex2f(positionOfScaledTexture.x, -sizeOfScaledTexture.height);
+			//br
+			glColor3f(1.0f, 1.0f, 1.0f);
+			glTexCoord2f(1.0f, 0.0f);
+			glVertex2f(sizeOfScaledTexture.width, -sizeOfScaledTexture.height);
+			//tr
+			glColor3f(1.0f, 1.0f, 1.0f);
+			glTexCoord2f(1, 1);
+			glVertex2f(sizeOfScaledTexture.width, -positionOfScaledTexture.y);
+			// tl
+			glColor3f(1.0f, 1.0f, 1.0f);
+			glTexCoord2f(0, 1);
+			glVertex2f(positionOfScaledTexture.x, -positionOfScaledTexture.y);
+
+			glEnd();
+		}
+		glEndList();
+	}
+
+	inline void PixelMode::Render()
+	{
+		Vector2i currentWindowSize;
+
+		currentWindowSize = enginePointer->GetWindowSize();
+
+		// If window size has changed
+		if ((currentWindowSize.width != _windowSize.width) || (currentWindowSize.height != _windowSize.height))
+		{
+			_windowSize = currentWindowSize;
+
+			// rebuild display list
+			_ScaleTextureToWindow();
+		}
+
+		// Cope video buffer to gpu
+		_UpdateFrameBuffer();
+
+		// Draw the quad
+		glBindTexture(GL_TEXTURE_2D, _frameBuffer[_currentBuffer].bind);
+		glCallList(_compiledQuad);
+		glBindTexture(GL_TEXTURE_2D, 0);
+
+	}
+
+	inline void PixelMode::Clear(const Color &color)
+	{
+		std::fill_n(_video, _bufferSize.width * _bufferSize.height, color.packed);
+	}
+
+	inline void PixelMode::Clear()
+	{
+		memset(_video, 0, (size_t)_bufferSize.width * (size_t)_bufferSize.height * sizeof(uint32_t));
+	}
+
+	inline void PixelMode::Pixel(const uint32_t x, const uint32_t y, const game::Color& color)
+	{
+		_video[y * _bufferSize.width + x] = color.packed;
 	}
 }
--- OpenGL-Window/Main.cpp
+++ OpenGL-Window/Main.cpp
@@ -13,9 +13,10 @@ public:
 	game::ShaderGL shader;
 	game::Terminal terminal; // error 6 when clicking x button to close
 	game::Color whitecol = { 1.0f, 1.0f, 1.0f, 1.0f };
+	game::PixelMode pixelMode;
 	
 	// pixel mode stuff
-	game::Texture2dGL createdTexture;
+	//game::Texture2dGL createdTexture;
 	uint32_t fullScreenTri;
 	uint32_t* video = nullptr;
 	game::Vector2i bufferSize = { 320,240 };
@@ -68,22 +69,6 @@ public:
 		}
 		whitecol.Set(1.0f, 0.0f, 1.0f, 1.0f);
 
-		// pixel mode stuff
-		createdTexture.width = bufferSize.width;
-		createdTexture.height = bufferSize.height;
-		createdTexture.componentsPerPixel = 4;
-		createdTexture.filterType = game::TextureFilterType::Point;
-		if (!CreateTexture(createdTexture))
-		{
-			logger->Error(game::lastError);
-		}
-		else
-		{
-			logger->Write("Texture created!");
-		}
-		// end pixel mode
-
-
 		// Setup OpenGL
 		glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
 		glEnable(GL_TEXTURE_2D);
@@ -92,27 +77,17 @@ public:
 		//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		glEnable(GL_CULL_FACE);
 
-		// will be part of pixel mode stuff
-		// after being able to check window size change
-		fullScreenTri = glGenLists(1);
-		glNewList(fullScreenTri, GL_COMPILE);
-		{
-			glBegin(GL_TRIANGLES);
-			{
 
-			}
-			glEnd();
+		if (!pixelMode.Initialize({ 320, 240 }))
+		{
+			logger->Error(game::lastError);
 		}
-		glEndList();
 	}
 
 	void Shutdown()
 	{
 		UnLoadTexture(texture);
 		UnLoadShader(shader);
-		// pixel mode stuff
-		UnLoadTexture(createdTexture);
-		if (video) delete[] video;
 	}
 
 	void Update(const float_t msElapsed)
@@ -127,118 +102,17 @@ public:
 			StopEngine();
 		}
 	}
-	// pixel mode stuff
-	void UpdateFrameBuffer()
-	{
-		// needs to double buffer
-		glBindTexture(GL_TEXTURE_2D, createdTexture.bind);
-		// Swap texture to draw to
-		//current++;
-		//if (current > 1) current = 0;
-
-		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, bufferSize.width, bufferSize.height, GL_RGBA, game::systemInfo.gpuInfo.internalPixelType, (GLvoid*)video);  // for intel 
-		glBindTexture(GL_TEXTURE_2D, 0);
-	}
-	// pixel mode stuff
-	void Clear(const uint32_t color)
-	{
-		std::fill_n(video, bufferSize.width * bufferSize.height, color);
-	}
-	// pixel mode stuff
-	void Clear()
-	{
-		memset(video, 0, (size_t)bufferSize.width * (size_t)bufferSize.height * sizeof(uint32_t));
-	}
-	// pixel mode stuff
-	void Pixel(const uint32_t x, const uint32_t y, const game::Color &color)
-	{
-		video[y * bufferSize.width + x] = color.packed;
-	}
 
 	void Render(const float_t msElapsed)
 	{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
 		// pixel mode stuff
-		Clear(whitecol.packed);
+		pixelMode.Clear(whitecol);
 		for (int i = 10; i < 100; i++)
-			Pixel(i, 10, { 1.0f, 1.0f, 1.0f, 1.0f });
-		UpdateFrameBuffer();
+			pixelMode.Pixel(i, 10, { 1.0f, 1.0f, 1.0f, 1.0f });
 
-		glBindTexture(GL_TEXTURE_2D, createdTexture.bind);
-		//glCallList(fullScreenTri);
-
-		// ---------- Begin scaling of quad
-		game::Vector2i windowSize = GetWindowSize();
-
-		// scale to window size
-		game::Vector2f positionOfScaledTexture;
-		game::Vector2f scale;
-		game::Vector2f sizeOfScaledTexture;
-		float_t tempScale = 0.0f;
-
-
-		if (windowSize.height < windowSize.width)
-		{
-			scale.y = (float_t)windowSize.height * createdTexture.oneOverHeight;
-			tempScale = (float_t)windowSize.width * createdTexture.oneOverWidth;
-			if (tempScale > scale.y)
-			{
-				scale.x = scale.y;
-			}
-			else
-			{
-				scale.x = scale.y = tempScale;
-				positionOfScaledTexture.y = ((windowSize.height >> 1) - ((float_t)createdTexture.height * scale.y / 2.0f));
-			}
-			positionOfScaledTexture.x = ((windowSize.width >> 1) - ((float_t)createdTexture.width * scale.x / 2.0f));
-		}
-		else if (windowSize.height > windowSize.width)
-		{
-			scale.x = (float_t)windowSize.width * createdTexture.oneOverWidth;
-			scale.y = scale.x;
-			positionOfScaledTexture.y = ((windowSize.height >> 1) - ((float_t)createdTexture.height * scale.y / 2.0f));
-		}
-		else
-		{
-			scale = { 1.0f, 1.0f };
-		}
-
-		// Set the size of the scaled texture
-		sizeOfScaledTexture.width = positionOfScaledTexture.x + (createdTexture.width * scale.x);
-		sizeOfScaledTexture.height = positionOfScaledTexture.y + (createdTexture.height * scale.y);
-
-		// Homoginize the scaled rect to -1 to 1 range using
-		// position.x = position.x * 2.0 / width - 1.0
-		// position.y = position.y * 2.0 / height - 1.0;
-		positionOfScaledTexture.x = (positionOfScaledTexture.x * 2.0f / (float_t)windowSize.width) - 1.0f;
-		positionOfScaledTexture.y = (positionOfScaledTexture.y * 2.0f / (float_t)windowSize.height) - 1.0f;
-		sizeOfScaledTexture.width = ((float_t)sizeOfScaledTexture.width * 2.0f / (float_t)windowSize.width) - 1.0f;
-		sizeOfScaledTexture.height = ((float_t)sizeOfScaledTexture.height * 2.0f / (float_t)windowSize.height) - 1.0f;
-
-		// ---------- End scaling of quad
-
-
-		// Draw the quad needs to be a drawlist that only updates on window size change
-		glBegin(GL_QUADS);
-		//bl
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glTexCoord2f(0, 0);
-		glVertex2f(positionOfScaledTexture.x, -sizeOfScaledTexture.height);
-		//br
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glTexCoord2f(1.0f, 0.0f);
-		glVertex2f(sizeOfScaledTexture.width, -sizeOfScaledTexture.height);
-		//tr
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glTexCoord2f(1, 1);
-		glVertex2f(sizeOfScaledTexture.width, -positionOfScaledTexture.y);
-		// tl
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glTexCoord2f(0, 1);
-		glVertex2f(positionOfScaledTexture.x, -positionOfScaledTexture.y);
-
-		glEnd();
+		pixelMode.Render();
 	}
 };
 
