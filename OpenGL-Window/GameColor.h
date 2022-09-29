933268eb3363664126db576d4d2f380b11c8de4d
ImGelling <47453119+imgelling@users.noreply.github.com>
2022-09-28 22:36:54 -0400

    Update GameColor.h
    Making more Color methods

--- OpenGL-Window/GameColor.h
+++ OpenGL-Window/GameColor.h
@@ -16,16 +16,15 @@ namespace game
 		uint32_t packed;
 
 		Color();
-		~Color();
 		Color(const float_t r, const float_t g, const float_t b, const float_t a);
-		//Color(const double_t r, const double_t g, const double_t b, const double_t a);
+		Color(const double_t r, const double_t g, const double_t b, const double_t a);
 		//Color(const uint32_t r, const uint32_t g, const uint32_t b, const uint32_t a);
 		//Color(const float_t r, const float_t g, const float_t b);
 		//Color(const double_t r, const double_t g, const double_t b);
 		//Color(const uint32_t r, const uint32_t g, const uint32_t b);
 
 		void Set(const float_t r, const float_t g, const float_t b, const float_t a);
-		//void Set(const double_t r, const double_t g, const double_t b, const double_t a);
+		void Set(const double_t r, const double_t g, const double_t b, const double_t a);
 		//void Set(const uint32_t r, const uint32_t g, const uint32_t b, const uint32_t a);
 		//void Set(const float_t r, const float_t g, const float_t b);
 		//void Set(const double_t r, const double_t g, const double_t b);
@@ -49,29 +48,10 @@ namespace game
 		packed = a << 24;
 	}
 
-	inline Color::~Color()
-	{
-
-	}
-
+	// ----- Begin float_t
 	inline Color::Color(const float_t r, const float_t g, const float_t b, const float_t a)
 	{
 		Set(r, g, b, a);
-		this->r = (uint32_t)(r * 255.0f);
-		this->g = (uint32_t)(g * 255.0f);
-		this->b = (uint32_t)(b * 255.0f);
-		this->a = (uint32_t)(a * 255.0f);
-		this->rf = r;
-		this->gf = g;
-		this->bf = b;
-		this->af = a;
-
-		// this may need to be changed for amd/nvidia
-		uint32_t packedR = this->r << 0;
-		uint32_t packedG = this->g << 8;
-		uint32_t packedB = this->b << 16;
-		uint32_t packedA = this->a << 24;
-		packed = ((packedA) | (packedR) | (packedG) | (packedB));
 	}
 
 	void Color::Set(const float_t r, const float_t g, const float_t b, const float_t a)
@@ -91,7 +71,35 @@ namespace game
 		uint32_t packedA = this->a << 24;
 		packed = ((packedA) | (packedR) | (packedG) | (packedB));
 	}
+	// ----- End float_t
 
+	// Begin double_t
+	inline Color::Color(const double_t r, const double_t g, const double_t b, const double_t a)
+	{
+		Set(r, g, b, a);
+	}
+
+	inline void Color::Set(const double_t r, const double_t g, const double_t b, const double_t a)
+	{
+		this->r = (uint32_t)(r * 255.0f);
+		this->g = (uint32_t)(g * 255.0f);
+		this->b = (uint32_t)(b * 255.0f);
+		this->a = (uint32_t)(a * 255.0f);
+		this->rf = (float_t)r;
+		this->gf = (float_t)g;
+		this->bf = (float_t)b;
+		this->af = (float_t)a;
+
+		uint32_t packedR = this->r << 0;
+		uint32_t packedG = this->g << 8;
+		uint32_t packedB = this->b << 16;
+		uint32_t packedA = this->a << 24;
+		packed = ((packedA) | (packedR) | (packedG) | (packedB));
+	}
+	// End double_t
+
+
+	// Defined standard colors (full alpha)
 	namespace Colors
 	{
 		const Color White(1.0f, 1.0f, 1.0f, 1.0f);
@@ -101,6 +109,6 @@ namespace game
 		const Color Black(0, 0, 0, 1.0f);
 		const Color Pink(1.0f, 0, 1.0f, 1.0f);
 		const Color Gray(0.5f, 0.5f, 0.5f, 1.0f);
-		const Color DarkGray(0.25f, 0.25f, 0.25, 1.0f);
+		const Color DarkGray(0.25f, 0.25f, 0.25f, 1.0f);
 	}
 }
