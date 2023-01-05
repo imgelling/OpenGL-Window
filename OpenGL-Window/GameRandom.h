2d0c75b4c138af9c89b4c4a366a813f0de5fa921
ImGelling <47453119+imgelling@users.noreply.github.com>
2023-01-05 00:43:47 -0500

    Code cleanup

--- OpenGL-Window/GameRandom.h
+++ OpenGL-Window/GameRandom.h
@@ -8,50 +8,50 @@ namespace game
 	{
 	public:
 		GameRandom();
-		void SetSeed(const unsigned int);
+		void SetSeed(const uint32_t seed);
 		uint32_t GetSeed();
 		uint32_t Rnd();
-		uint32_t RndRange(const unsigned int, const unsigned int);
+		uint32_t RndRange(const uint32_t min, const uint32_t max);
 		~GameRandom();
 	private:
-		unsigned int seed;
-		std::mt19937 generator;
+		unsigned int _seed;
+		std::mt19937 _mt19937Generator;
 
 	};
 
 	GameRandom::GameRandom()
 	{
 		std::random_device rd;
-		seed = rd() ^
+		_seed = rd() ^
 			(
 				(std::mt19937::result_type)
 				std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count() +
 				(std::mt19937::result_type)
 				std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count()
 				);
-		generator.seed(seed);
+		_mt19937Generator.seed(_seed);
 	}
 
-	inline void GameRandom::SetSeed(const unsigned int seed)
+	inline void GameRandom::SetSeed(const uint32_t seed)
 	{
-		this->seed = seed;
-		generator.seed(seed);
+		this->_seed = seed;
+		_mt19937Generator.seed(seed);
 	}
 
 	inline uint32_t GameRandom::GetSeed()
 	{
-		return seed;
+		return _seed;
 	}
 
 	inline uint32_t GameRandom::Rnd()
 	{
-		return generator();
+		return _mt19937Generator();
 	}
 
-	inline uint32_t GameRandom::RndRange(const unsigned int min, const unsigned int max)
+	inline uint32_t GameRandom::RndRange(const uint32_t min, const uint32_t max)
 	{
 		std::uniform_int_distribution<unsigned> distrib(min, max);
-		return distrib(generator);
+		return distrib(_mt19937Generator);
 	}
 
 	GameRandom::~GameRandom()
